41dd75a24d48bc18ac36d1d131c3887bf7a15696
Abhishek Hegde <ahegde2021@gmail.com>
2023-11-24 00:19:46 +0530

    Navbar , sign in , sign out , home page.
    automatic redirect if user was signed in .
    
    nested layout inside notes route to have navbar visible at alll times inside rout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ai-Note-Taking-App",
-  description: "Intelligent note taking app",
+  title: "FlowBrain",
+  description: "The intelligent note-taking app",
 };
 
 export default function RootLayout({
